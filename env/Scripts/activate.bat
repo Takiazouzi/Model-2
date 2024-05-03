1e7030a20d4cdcb4cb130abde491b7224761d53e
Takiazouzi <takiazouzi0@gmail.com>
2024-05-03 02:04:32 +0100

    naseyaaaaaaaaa

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\takia\OneDrive\Desktop\MediaNet\Model 4\Model-2\enc
+set VIRTUAL_ENV=C:\Users\takia\OneDrive\Desktop\MediaNet\Model 4\Model-2\env
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(enc) %PROMPT%
+set PROMPT=(env) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
